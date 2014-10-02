c81ed354a993f47c55ae0d31a377f399cda2c109
Alejandro Vidal Rodriguez <goofyahead@gmail.com>
2014-10-02 16:12:39 +0200

    changes

--- app.js
+++ app.js
@@ -18,19 +18,19 @@ app.set('port', process.env.PORT || 3000);
 
 var apps = {};
 
-setInterval(function () {
-    for (apiKey in apps) {
-        for( room in apiKey) {
-            setImmediate(function ( currentApiKey, currentRoom) {
-                if (apps[currentApiKey][currentRoom].participants.length == 0 && apps[currentApiKey][currentRoom].flagged == true) {
-                    delete apps[currentApiKey][currentRoom];
-                } else if (apps[currentApiKey][currentRoom].participants.length == 0) {
-                    apps[currentApiKey][currentRoom].flagged = true;
-                }
-            }, apiKey, room);
-        }
-    }
-}, CHECK_DEAD_ROOMS_TIMEOUT);
+// setInterval(function () {
+//     for (apiKey in apps) {
+//         for( room in apiKey) {
+//             setImmediate(function ( currentApiKey, currentRoom) {
+//                 if (apps[currentApiKey][currentRoom].participants.length == 0 && apps[currentApiKey][currentRoom].flagged == true) {
+//                     delete apps[currentApiKey][currentRoom];
+//                 } else if (apps[currentApiKey][currentRoom].participants.length == 0) {
+//                     apps[currentApiKey][currentRoom].flagged = true;
+//                 }
+//             }, apiKey, room);
+//         }
+//     }
+// }, CHECK_DEAD_ROOMS_TIMEOUT);
 
 io.sockets.on('connection', function (socket) {
 
@@ -45,23 +45,36 @@ io.sockets.on('connection', function (socket) {
         apps[socket.apiKey] = {};
         apps[socket.apiKey][socket.roomId] = { participants: [ data.userName ], roomInfo : data.roomInfo, flagged : false };  
         console.log('joining room ' + socket.apiKey + ":" + socket.roomId);    
-        socket.join(socket.apiKey + ":" + socket.roomId);
+        //socket.join(socket.apiKey + ":" + socket.roomId);
     });
 
     socket.on('joinRoom', function (data){ //data = { roomId: '1', apiKey : '1', userName: '' }
         console.log('Joining to room: '.green, data);
+        data = JSON.parse(data);
         socket.roomId = data.roomId;
         socket.apiKey = data.apiKey;
         socket.userName = data.userName;
-        apps[socket.apiKey][socket.roomId].participants.push(data.userName);
-        //set flagged to false in case it was empty for some time;
-        apps[socket.apiKey][socket.roomId].flagged = false;
-        //Join this room
-        socket.join(socket.apiKey + ":" + socket.roomId);
-        //notifiy all a new client appeared
-        socket.broadcast.to(socket.apiKey + ":" + socket.roomId).emit('joined', { user : socket.playerName });
-        //send back information to only this client of the room
-        socket.emit('roomInfo', { participants: apps[socket.apiKey][socket.roomId].participants, roomInfo : apps[socket.apiKey][socket.roomId].roomInfo});
+        if (apps[socket.apiKey] && apps[socket.apikey][socket.roomId]) {
+            apps[socket.apiKey][socket.roomId].participants.push(data.userName);
+            //set flagged to false in case it was empty for some time;
+            apps[socket.apiKey][socket.roomId].flagged = false;
+            //Join this room
+            socket.join(socket.apiKey + ":" + socket.roomId);
+            //notifiy all a new client appeared
+            socket.broadcast.to(socket.apiKey + ":" + socket.roomId).emit('joined', socket.playerName );
+            //send back information to only this client of the room
+            socket.emit('roomInfo', { participants: apps[socket.apiKey][socket.roomId].participants, roomInfo : apps[socket.apiKey][socket.roomId].roomInfo});
+        } else {
+            console.log('joining an unexistant room'.red);
+        }
+    });
+
+    socket.on('me', function (data){ // change my info {userName: 'foo'}
+        data = JSON.parse(data);
+        _.without(apps[socket.apiKey][socket.roomId].participants, socket.userName);
+        socket.userName = data.userName;
+        apps[apiKey][socket.roomId].participants.push(socket.userName);
+        ocket.emit('roomInfo', { participants: apps[socket.apiKey][socket.roomId].participants, roomInfo : apps[socket.apiKey][socket.roomId].roomInfo});
     });
 
     socket.on('message', function (data) { //any object
@@ -75,7 +88,7 @@ io.sockets.on('connection', function (socket) {
         if (apps[socket.apiKey] && apps[socket.apiKey][socket.roomId]) {
             apps[socket.apiKey][socket.roomId].participants = _.without(apps[socket.apiKey][socket.roomId].participants, socket.userName);
         }
-        socket.volatile.broadcast.to(socket.apiKey + ":" + socket.roomId).emit('disconnected', { user : socket.playerName });
+        socket.volatile.broadcast.to(socket.apiKey + ":" + socket.roomId).emit('disconnected', socket.playerName);
     });
 
     socket.on('destroyRoom', function (data) { // deletion of room and this socket { roomId: '1', apiKey : '1', userName: '' }
